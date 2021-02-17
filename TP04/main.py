ba67fa002a185039a0cb15752230b40a7d6533f2
franciscocamellon <franciscocamellon@gmail.com>
2021-02-17 12:45:23 -0300

    Adds one more page

--- TP04/main.py
+++ TP04/main.py
@@ -25,7 +25,7 @@ class MainWindow():
         self.DISPLAY_NAME = pygame.display.set_caption('Análise do Sistema')
         self.finish = False
         self.FPS_COUNT = 60
-        self.count = 0
+        self.count = 4
         self.fontIntro = pygame.font.SysFont("Times New Roman", 30)
 
     def drawIntro(self, screen):
@@ -87,6 +87,14 @@ class MainWindow():
                 self.SIZE[0],
                 self.SIZE[1],
                 System_Info()._info_files(), 10)
+                
+        elif self.count == 5:
+            pass
+            Elements_Components().shows_pid_info_text(
+                screen,
+                self.SIZE[0],
+                self.SIZE[1],
+                System_Info()._pid_info(), 10)
 
     def init_class(self):
         while not self.finish:
